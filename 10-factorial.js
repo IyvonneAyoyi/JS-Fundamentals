573153f17a5578dc44a1d66aa4ea879da3567c5b
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-07-12 10:18:42 +0300

    Update script to handle missing and undefined values

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,17 @@
 const num = parseInt(process.argv[2]);
 
 function factorial(n) {
-  if (n === 1){
+    if (isNaN(n)) {
+    return 1;
+  } else if (n < 0) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  }
+  else if (n === 1){
     return 1;
   }else {
-  return n * factorial(n - 1);
+   return n * factorial(n - 1);
 }
 }
 console.log(factorial(num));
